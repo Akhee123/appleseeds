b69cfd01f8faf63692a9eb9e785584535400819c
Akhee123 <mabuirmelah@gmail.com>
2023-01-08 14:44:10 +0200

    1.1 js task

---
+++ JS/tasks/solutions/1.1_primitive_data_types/app.js
@@ -0,0 +1,18 @@
+
+// Declaring variables
+let str = "This is a string";
+let num = 1;
+let obj = Math;
+let bool = true;
+let a = null;
+let b = undefined;
+let c = "$";
+
+// Logging variables
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof obj);
+console.log(typeof bool);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
