0b68921d5a1e150cb1d2eae006ed40e520048b67
Akhee123 <mabuirmelah@gmail.com>
2023-01-08 15:35:28 +0200

    2.1 JS task

---
+++ JS/tasks/solutions/2.2_functions/app.js
@@ -0,0 +1,4 @@
+function tellFortune(jobTitle, location, partnerName, numberOfChildren)
+{
+    return `You will be ${jobTitle} in ${location} married to ${partnerName} with ${numberOfChildren} children`;
+}
