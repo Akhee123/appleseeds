aecee162841cf7c0de6c9c1585594e604380e787
Akhee123 <mabuirmelah@gmail.com>
2023-01-11 12:59:03 +0200

    5.4 JS task

---
+++ JS/tasks/solutions/5.4_dark_or_light_color/app.js
@@ -0,0 +1,20 @@
+
+function darkOrLight(color){
+    switch(color){
+
+        case "yellow":
+        case "pink":
+        case "orange":
+            console.log("Light Color");
+            break;
+        
+        case "blue":
+        case "purple":
+        case "brown":
+            console.log("Dark Color");
+            break;
+        
+        default:
+            console.log("Unknown color");
+    }
+}
