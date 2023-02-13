59e1ced76e99a563db4bf2fd3fc24e0de81a773a
Akhee123 <mabuirmelah@gmail.com>
2023-02-13 11:36:11 +0200

    React Components

---
+++ React/udemy/my-first-react/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import ExepenseItem from './components/ExpenseItem'
+
+function App() {
+
+  return (
+    <div className="App">
+      <ExepenseItem></ExepenseItem>
+    </div>
+  )
+}
+
+export default App
