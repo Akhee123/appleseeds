6281bd2508570f0fb7848a1585abb2eac3de8714
Akhee123 <mabuirmelah@gmail.com>
2023-02-21 11:05:45 +0200

    Task React 13.1

---
+++ React/tasks/solutions/13.1_favorite_color/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FavoriteColor from "./components/FavoriteColor";
+
+function App() {
+
+  return (
+    <div className="App">
+      <FavoriteColor />
+    </div>
+  )
+}
+
+export default App
