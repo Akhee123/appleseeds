34f2f6d9bbe833446bd315cf7c0a47375e77ca21
akhee123 <mabuirmelah@gmail.com>
2023-03-14 21:50:01 +0200

    Task file_system

---
+++ Node/tasks/solutions/file-system/app.js
@@ -0,0 +1,9 @@
+
+const fs = require('fs');
+
+fs.writeFileSync('test.txt', 'This is a test file!'); //Create
+fs.copyFileSync('test.txt', 'copy.txt'); // Copy
+fs.renameSync('copy.txt', 'rename.txt'); // Rename
+
+console.log(fs.readdirSync('./')); // List content of dir
+
---
+++ Node/udemy/notes-app/app.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+
+// fs.writeFileSync('notes.txt', 'Made by Node.js');
+fs.appendFileSync('notes.txt', 'THIS IS APPENDED');
